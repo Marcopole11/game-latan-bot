7b03afad38fd6e33985653c32da80713447db37e
Marcopole11 <35684962+Marcopole11@users.noreply.github.com>
2018-04-25 11:43:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
 });
-const autorole = require("./autoroles.json");
+const autorole = require("./readaptable.json");
 
 var prefix = 'mL+';
 client.on('message', message => {
